a59bbf58bfa6f739e3984cbb255f7caa839c94d8
Cyril Scetbon <cscetbon@gmail.com>
2020-06-09 22:35:46 -0400

    Remove unused code

--- regenerate.sh
+++ regenerate.sh
@@ -1,22 +1,23 @@
 #! /bin/bash
 
-echo Generating Cassandra Sidecar Go client
-export wv_go_path="module github.com\/erdrix\/cassandrasidecar-go-client"
-export workspace_dir=$(pwd)
+export wv_go_path="module github.com/cscetbon/cassandrasidecar-go-client"
+export workspace_dir=$PWD
 export wv_tmp_dir=${workspace_dir}/pkg
+
+echo Generating Cassandra Sidecar Go client
+
 rm -fr ${workspace_dir}/pkg && mkdir -p ${workspace_dir}/pkg/cassandrasidecar
 cd resources/client_gen
 ./generate_api_client.sh
-
-cd ../../
+cd -
 
 echo Cleaning Cassandra Sidecar Go client
-mv ${wv_tmp_dir}/cassandrasidecar/go-client/* ${wv_tmp_dir}/cassandrasidecar && rm -fr ${wv_tmp_dir}/cassandrasidecar/go-client
+mv ${wv_tmp_dir}/cassandrasidecar/go-client/* ${wv_tmp_dir}/cassandrasidecar && rm -rf ${wv_tmp_dir}/cassandrasidecar/go-client
 rm -f ${wv_tmp_dir}/cassandrasidecar/go.sum
 rm -f ${wv_tmp_dir}/cassandrasidecar/git_push.sh
 
 echo Moving documentations files to root dirs
 rm -fr docs && mv ${wv_tmp_dir}/cassandrasidecar/docs .
-rm -fr README.md && mv ${wv_tmp_dir}/cassandrasidecar/README.md .
-rm -fr go.mod && mv ${wv_tmp_dir}/cassandrasidecar/go.mod .
-sed -i.bak "1s/.*/$wv_go_path/" go.mod && echo "go 1.13" >> go.mod
+mv ${wv_tmp_dir}/cassandrasidecar/README.md .
+mv ${wv_tmp_dir}/cassandrasidecar/go.mod .
+sed -i.bak "1s#.*#$wv_go_path#" go.mod && echo "go 1.14" >> go.mod
--- resources/client_gen/generate_api_client.sh
+++ resources/client_gen/generate_api_client.sh
@@ -13,13 +13,9 @@ export wv_client_name=${wv_client_name:-cassandrasidecar}
 export wv_codegen_filename=${wv_codegen_filename:-swagger-codegen-cli-3.0.20.jar}
 export wv_tmp_dir=${wv_tmp_dir:-${HOME}/Projects/tmp}
 export wv_client_dir=${wv_tmp_dir}/${wv_client_name}
-export wv_mustache_dir=./swagger_templates
-export wv_api_def_dir=./api_defs
 
 export wv_openapi_yaml_url=https://raw.githubusercontent.com/instaclustr/cassandra-sidecar/master/spec.yaml
 export wv_generator_url=http://api.openapi-generator.tech/api/gen
-
-export wv_swagger_def=$(ls ${wv_api_def_dir} | grep ${wv_client_name} | sort -V | tail -1)
 
 echo Downloading go client for ${wv_openapi_yaml_url} from ${wv_generator_url}
 
@@ -29,13 +25,12 @@ code=$(curl -X POST "${wv_generator_url}/clients/go" \
   -d "{ \"openAPIUrl\": \""${wv_openapi_yaml_url}"\", \"options\": { \"packageName\": \""${wv_client_name}"\", \"packageVersion\": \"1.0.0\" }}" | jq --raw-output '.code')
 
 
-echo Downloading ${wv_codegen_filename}
+echo Downloading generated code
 
 mkdir tmp
 wget "${wv_generator_url}/download/${code}" -P tmp
 rm -fr ${wv_client_dir}
 unzip "tmp/${code}" -d ${wv_client_dir}
-
 rm -fr tmp
 
 
